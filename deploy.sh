3568ee68c1c60d5cbd8844a1cb68d922d06a9338
jamireyes <35447073+jamireyes@users.noreply.github.com>
2022-11-21 19:58:08 +0800

    New Deployment

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+GIT_REPO_URL=$(git config --get remote.origin.url)
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'New Deployment'
+git push --set-upstream -f $GIT_REPO_URL master
+
+cd -
